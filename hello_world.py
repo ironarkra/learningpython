952379feceae6aaa72c5a655a2ae840b9ba6f8c5
ironarkra <morenoalexa934@gmail.com>
2020-05-25 09:47:09 -0700

    setting up repository

---
+++ hello_world.py
@@ -0,0 +1,30 @@
+# This is  a comment.
+# Lines that start with '#' are called comments .
+# These lines are not instructions.
+# They are for humans to read so as to better understand your code.
+
+# Milestone 0: Hellow world
+#print("Scoopidy Whoop")
+
+# Milestone 1: Comment out the previous milestone and print 5 statements
+
+# Milestone 2a: Print "Poop" 1000 times!
+
+# Milestone 2b: create a string variable and print it.
+
+# Milestone 3: Print "poop" / "fart" alternating from 0-99 (100 times total), that is on even iterations print "poop" and on odd iterations print "fart" (so 50 of each).
+
+#Milestore 1: [...] Print 5 statements 
+print("Ali is dancing in a field of sunflowers underneath a gray sky.")
+print("The world around him forgotten, he hears a whisper weave itself in the wind and speak his name.")
+print("A perfect melody lifts his small frame over the curiously swaying heads looking up at him.")
+print("Breaking through the clouds he watches as the rain begins to fall beneath him.")
+print("A woman below runs over the sunflowers forgetfully singing a familiar song as Ali slowly falls asleep.")
+
+#Milestone 2a: Print "Poop" 1000 times!
+#for i in range(1000):
+#    print("poop")
+
+#Milestone 2b: Create a string variable and print it.
+string= "Kevin, te quiero comer."
+    print string.capitalize
